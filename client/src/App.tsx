b5010c4a4be203d282f3233d5c7104e392dafc7f
kacperborowieckb <kacperborowiec.kb@gmail.com>
2023-09-18 15:20:50 +0200

    added nedded routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,16 +3,25 @@ import { CssBaseline, Experimental_CssVarsProvider as CssVarsProvider } from '@m
 import { Routes, Route } from 'react-router';
 import Layout from './components/layout/Layout';
 import MainPage from './pages/MainPage';
+import WelcomePage from './pages/WelcomePage';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import RequireAuth from './components/require-auth/RequireAuth';
+import Missing from './pages/Missing';
 
 function App() {
   return (
     <CssVarsProvider theme={theme}>
       <CssBaseline enableColorScheme />
       <Routes>
-        <Route>
-          <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<WelcomePage />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route element={<RequireAuth />}>
             <Route index element={<MainPage />} />
           </Route>
+          <Route path="*" element={<Missing />} />
         </Route>
       </Routes>
     </CssVarsProvider>
---
+++ client/src/components/require-auth/RequireAuth.tsx
@@ -0,0 +1,12 @@
+import { Navigate, Outlet, useLocation } from 'react-router';
+import { selectCurrentToken } from '../../features/auth/authSlice';
+import { useAppSelector } from '../../hooks/useAppSelector';
+
+const RequireAuth = () => {
+  const token = useAppSelector(selectCurrentToken);
+  const location = useLocation();
+
+  return token ? <Outlet /> : <Navigate to={'/signin'} state={{ from: location }} replace />;
+};
+
+export default RequireAuth;
