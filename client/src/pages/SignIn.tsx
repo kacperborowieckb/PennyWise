ee00856064c49d5570093ae8d2f501cf1fdce227
kacperborowieckb <kacperborowiec.kb@gmail.com>
2023-09-18 19:44:53 +0200

    sign in error handling

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -1,14 +1,20 @@
-import { Box, Button, Stack, TextField } from '@mui/material';
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { Alert, Box, Button, Snackbar, Stack, TextField } from '@mui/material';
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import signInPageImg from '../assets/signin-page-img.svg';
 import { useLoginMutation } from '../features/auth/authApiSlice';
 import { useAppDispatch } from '../hooks/useAppDispatch';
 import { AuthSliceInitialState, setCredentials } from '../features/auth/authSlice';
 import { useNavigate } from 'react-router';
 
+type Error = {
+  status: number;
+  data: unknown;
+};
+
 const SignIn = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState<string | null>(null);
   const [login, { isLoading }] = useLoginMutation();
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
@@ -16,28 +22,37 @@ const SignIn = () => {
   const handleUsernameChange = (e: ChangeEvent<HTMLInputElement>) => setUsername(e.target.value);
   const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value);
 
+  const clearInputs = () => {
+    setUsername('');
+    setPassword('');
+  };
+  const clearError = () => setError(null);
+
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     try {
       const userData = (await login({ username, password }).unwrap()) as AuthSliceInitialState;
       dispatch(setCredentials({ ...userData }));
-      setUsername('');
-      setPassword('');
+      clearInputs();
       navigate('/');
     } catch (err) {
-      //   if (!err?.originalStatus) {
-      //     // isLoading: true until timeout occurs
-      //     setErrMsg('No Server Response');
-      // } else if (err.originalStatus === 400) {
-      //     setErrMsg('Missing Username or Password');
-      // } else if (err.originalStatus === 401) {
-      //     setErrMsg('Unauthorized');
-      // } else {
-      //     setErrMsg('Login Failed');
-      // }
+      const knownError = err as Error;
+      if (!knownError?.status) {
+        setError('No Server Response');
+      } else if (knownError.status === 400) {
+        setError('Missing Username or Password');
+      } else if (knownError.status === 401) {
+        setError('Unauthorized');
+      } else {
+        setError('Login Failed');
+      }
     }
   };
+
+  useEffect(() => {
+    clearError();
+  }, [username, password]);
 
   return (
     <Box sx={{ height: '100%', display: 'flex', alignItems: 'center' }}>
@@ -50,10 +65,23 @@ const SignIn = () => {
           maxWidth: 300,
           gap: 2,
           border: '1px solid',
+          position: 'relative',
           borderColor: (theme) => theme.palette.primary.main,
           borderRadius: 2,
         }}
       >
+        {error && (
+          <Snackbar
+            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+            open={Boolean(error.length)}
+            autoHideDuration={4000}
+            onClose={clearError}
+          >
+            <Alert onClose={clearError} severity="error" sx={{ width: '100%' }}>
+              {error}
+            </Alert>
+          </Snackbar>
+        )}
         <img src={signInPageImg} alt="sign in illustration" style={{ height: 100 }} />
         <TextField
           variant="outlined"
