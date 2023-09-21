b33d4b2d54f3e6f9a6ef0888161e2341db4dd712
kacperborowieckb <kacperborowiec.kb@gmail.com>
2023-09-21 18:39:05 +0200

    basic overwie section design + basic pie chart design

--- client/src/components/budget/Budget.tsx
+++ client/src/components/budget/Budget.tsx
@@ -1,9 +1,52 @@
-import { Paper } from '@mui/material';
+import { Grid, Paper, Typography } from '@mui/material';
+import { PieChart } from '@mui/x-charts';
+import { categoriesTotalExpenses } from '../../helpers/mockCategories';
 
 const Budget = () => {
   return (
     <Paper sx={{ height: '100%', p: 2 }} elevation={6}>
-      Budget
+      <Grid container spacing={2} height={'100%'} p={2}>
+        <Grid item xs={6} container spacing={2} direction={'column'}>
+          <Grid item xs={8}>
+            <Typography component={'h1'} variant="h4">
+              4523.57$
+            </Typography>
+            <Typography component={'p'} variant="h6">
+              Total
+            </Typography>
+          </Grid>
+          <Grid item xs={4}>
+            Chart
+          </Grid>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography component={'h2'} variant="h5">
+            Expenses
+          </Typography>
+          <PieChart
+            width={210}
+            height={210}
+            colors={['red', 'orange', 'pink', 'blue', 'green']}
+            legend={{ hidden: true }}
+            series={[
+              {
+                data: [
+                  ...categoriesTotalExpenses.map((category) => ({
+                    label: category.categoryName,
+                    value: category.totalExpenses,
+                  })),
+                ],
+                innerRadius: 25,
+                outerRadius: 100,
+                paddingAngle: 3,
+                cornerRadius: 5,
+                cx: 100,
+                cy: 100,
+              },
+            ]}
+          />
+        </Grid>
+      </Grid>
     </Paper>
   );
 };
---
+++ client/src/helpers/mockCategories.ts
@@ -0,0 +1,7 @@
+export const categoriesTotalExpenses = [
+  { categoryName: 'Bills', totalExpenses: 200 },
+  { categoryName: 'Personal', totalExpenses: 300 },
+  { categoryName: 'Groceries', totalExpenses: 50 },
+  { categoryName: 'Travel', totalExpenses: 150 },
+  { categoryName: 'Health', totalExpenses: 10 },
+];
